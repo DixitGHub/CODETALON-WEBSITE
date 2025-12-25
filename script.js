f640b83e3af129e724b94a7087db692f7ddc0cb2
Dixit <dixitsaravithyan@gmail.com>
2025-12-26 01:03:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -106,3 +106,15 @@ document.addEventListener("keydown", function (e) {
     }
 });
 
+// selecting elements
+var sidenav = document.getElementById("sidenav")
+var closenav = document.getElementById("closenav")
+var menuicon = document.getElementById("menuicon")
+
+menuicon.addEventListener("click", function(){
+sidenav.style.right = 0;
+})
+
+closenav.addEventListener("click", function(){
+  sidenav.style.right = "-70%"
+})
